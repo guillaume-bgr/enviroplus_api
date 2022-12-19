75ed19d744340be7f755ae4c4faf4736e0da50e6
guillaume-bgr <guillaumebongrand.lamanu@gmail.com>
2022-12-19 11:03:23 +0100

    Ajout fichier redis.py

--- redis.py
+++ redis.py
@@ -1,4 +1,8 @@
 import redis
 r = redis.Redis()
 r.ping()
+r.xadd(name, fields, id='*', maxlen=None, approximate=True, nomkstream=False, minid=None, limit=None)
+# class StreamManager :
 
+# def appendStream() :
+#     r.xadd(name, fields, id='*', maxlen=None, approximate=True, nomkstream=False, minid=None, limit=None)
