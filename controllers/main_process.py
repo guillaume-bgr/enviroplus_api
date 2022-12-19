fd4cf8b5c48533aebc1d6ff2427a4c2a6749a05d
guillaume-bgr <guillaumebongrand.lamanu@gmail.com>
2022-12-19 15:44:25 +0100

    Imports

---
+++ controllers/main_process.py
@@ -0,0 +1,6 @@
+from ..services.stream_manager import StreamManager
+import all_in_one
+
+# streamManager = StreamManager()
+# # streamManager.appendStream('rasp_output', {'salut': 0.12, 'co': 0.25})
+# streamManager.getStream('rasp_output', '-', '+', 10)
