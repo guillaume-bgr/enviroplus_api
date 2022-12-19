ec35c507093a0c146c2502e506afe09ab036914d
guillaume-bgr <guillaumebongrand.lamanu@gmail.com>
2022-12-19 14:28:56 +0100

    Ajout stream et lecture stream

--- stream_manager.py
+++ stream_manager.py
@@ -1,8 +1,18 @@
+from typing import Union
 import redis
 r = redis.Redis()
-r.ping()
-# r.xadd(name, fields, id='*', maxlen=None, approximate=True, nomkstrea m=False, minid=None, limit=None)
-# class StreamManager :
+print(r.ping())
 
-# def appendStream() :
-#     r.xadd(name, fields, id='*', maxlen=None, approximate=True, nomkstream=False, minid=None, limit=None)
+class StreamManager :
+
+    def appendStream(self, name: str, fields: float) :
+        print(r.xadd(name, fields))
+        
+    def getStream(self, stream_name: str, start: Union['string', 'int'], end: Union['string', 'int'], count: int) :
+        print(r.xrevrange(stream_name, start, end, count))
+    
+streamManager = StreamManager()
+# streamManager.appendStream('rasp_output', {'salut': 0.12, 'co': 0.25})
+streamManager.getStream('rasp_output', '-', '+', 10)
+
+
