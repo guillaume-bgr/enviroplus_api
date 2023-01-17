68dc91e6461f948236a7ca8d6a4fc899b4ec70f8
guillaume-bgr <guillaumebongrand.lamanu@gmail.com>
2023-01-17 13:57:25 +0100

    Fonction prélèvement capteurs

--- main_process.py
+++ main_process.py
@@ -2,18 +2,13 @@ import controllers.all_in_one as aio
 from services.stream_manager import StreamManager
 import time
 
-# Initialisation stream
-stream = StreamManager()
+sm = StreamManager()
 
-def get_sensor_data():
-    print("Adding data to the stream..")
-    data = aio.get_stream_data()
-    stream.append_stream('rasp_output', data)
-    stream.get_stream('rasp_output', '-', '+', 1)
-    # Get sensor data
-    # stream.trim_stream('rasp_output', 0.2)
-    
+def main_process():
+    while True:
+        sm.append_stream('rasp_output', aio.get_stream_data())
+        print((sm.get_stream('rasp_output', '-', '+', 10)))
+        time.sleep(180)
 
-while True:
-    get_sensor_data()
-    time.sleep(180)
+main_process()
+
